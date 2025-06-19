f69606837dc63219c7184c34f6b3bba60fd11d34
y0sh104 <jf161004@gmail.com>
2025-06-19 21:40:43 +0200

    Now

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -45,7 +45,6 @@ public class Controller extends JFrame implements KeyListener, ActionListener, M
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
 	}
 
 
@@ -137,47 +136,34 @@ public class Controller extends JFrame implements KeyListener, ActionListener, M
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
 	}
 
 	/////////////////// Action Events ////////////////////////////////
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 	
 	/////////////////// Mouse Events ////////////////////////////////
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 	
 
